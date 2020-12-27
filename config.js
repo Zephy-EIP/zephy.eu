3e1e8d85bcc7ca4a3109daef2479a10772ff75bb
Benjamin Bourgeois <benjamin.bourgeois@epitech.eu>
2020-12-27 11:42:01 +0100

    :tada: add coming soon page

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Zephy', // <title>
+  manifestName: 'Eventually',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/images/website-icon.png',
+  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Zephy.eu',
+  subHeading:
+    "Zephy est un assistant personnalisable qui a pour but de faciliter la plannification de tous vos déplacements urbains. Il vous propose des trajets adapté à votre agenda, vos moyens de transports disponibles ainsi que le taux d'affluence dans les transports en commun.",
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/Zephy-EIP',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:contact@zephy.eu',
+    },
+  ],
+};
